ef08bd31ec9fc56d733a0360a96fa80009f7ecb4
gammablaze <gammablaze@gmail.com>
2022-06-01 19:00:38 +0100

    Updating main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,46 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests."
+  type        = number
+  default     = 8080
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #! /bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = var.server_port
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = var.server_port
+  }]
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server."
 }
