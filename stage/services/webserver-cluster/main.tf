020e4f255c9c241f9c7b160af4f370682d96678e
gammablaze <gammablaze@gmail.com>
2022-06-10 18:12:07 +0100

    Moving out module code.

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source                 = "../../../modules/services/webserver-cluster"
+  source                 = "github.com/gammablaze/tf_up_and_running_modules//webserver-cluster?ref=v0.0.1"
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-st8"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
